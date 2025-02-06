7792a7b36ce30f74f18e36126e3e777d179031d4
Hardik-Ahi <ahi.hardik@gmail.com>
2025-02-06 19:44:59 +0530

    minor change in adam, still does not work

--- optimizers.py
+++ optimizers.py
@@ -62,8 +62,8 @@ class Adam(SGD):
         self.bias_std = list()
         self.beta1 = beta1
         self.beta2 = beta2
-        self.beta1_power = self.beta1
-        self.beta2_power = self.beta2
+        self.beta1_power = beta1
+        self.beta2_power = beta2
 
         for i in self.model.weights:
             self.weight_mean.append(np.zeros(i.gradients.shape))
@@ -84,36 +84,37 @@ class Adam(SGD):
         gradients = self.model.weights[weight_index].gradients
 
         weight_momentum = self.weight_mean[weight_index]
-        weight_momentum = self.beta1 * weight_momentum - ((1 - self.beta2) * gradients)
-        self.weight_mean[weight_index] = weight_momentum  # accumulate un-normalized history of gradients
-        weight_m_normal = weight_momentum / (1 - self.beta1_power)  # normalize later
+        weight_momentum = self.beta1 * weight_momentum - ((1 - self.beta1) * gradients)
+        weight_m_normal = weight_momentum / (1 - self.beta1_power)
+        self.weight_mean[weight_index] = weight_m_normal
 
         weight_scale = self.weight_std[weight_index]
         weight_scale = self.beta2 * weight_scale + ((1 - self.beta2) * np.square(gradients))
-        self.weight_std[weight_index] = weight_scale
         weight_s_normal = weight_scale / (1 - self.beta2_power)
+        self.weight_std[weight_index] = weight_s_normal
 
-        return weight_m_normal / np.sqrt(weight_s_normal + epsilon)
+        return np.divide(weight_m_normal, np.sqrt(weight_s_normal + epsilon))
         
 
     def current_gradient_biases(self, layer_index):
         gradients = self.model.layers[layer_index].b_gradients
 
         bias_momentum = self.bias_mean[layer_index]
-        bias_momentum = self.beta1 * bias_momentum - ((1 - self.beta2) * gradients)
-        self.bias_mean[layer_index] = bias_momentum
+        bias_momentum = self.beta1 * bias_momentum - ((1 - self.beta1) * gradients)
         bias_m_normal = bias_momentum /  (1 - self.beta1_power)
+        self.bias_mean[layer_index] = bias_m_normal
 
         bias_scale = self.bias_std[layer_index]
         bias_scale = self.beta2 * bias_scale + ((1 - self.beta2) * np.square(gradients))
-        self.bias_std[layer_index] = bias_scale
         bias_s_normal = bias_scale / (1 - self.beta2_power)
+        self.bias_std[layer_index] = bias_s_normal
 
-        return bias_m_normal / np.sqrt(bias_s_normal + epsilon)
+        return np.divide(bias_m_normal, np.sqrt(bias_s_normal + epsilon))
     
     
     def on_pass(self):
         super().on_pass()  # yes, reset gradients, [but not iteration?]
+        #print(f"adam's on_pass, weight moments = {self.weight_mean}")
         self.iteration += 1
         self.beta1_power *= self.beta1
         self.beta2_power *= self.beta2
