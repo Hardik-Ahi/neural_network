2f2b60ccad7caadd7df9c0d4e32b05c27d68ef93
Hardik-Ahi <ahi.hardik@gmail.com>
2025-02-12 23:20:08 +0530

    initial working state of PLOTS! (history of applied gradients)

--- plotter.py
+++ plotter.py
@@ -6,31 +6,63 @@ import os, time
 class Plotter:
     def read_file(self, path):
         self.data = Logger.load_data(path)
+        if self.data is None:
+            print("unsucessful.")
+        else:
+            print("file read.")
     
-    def set_model_info(self, n_layers, n_weights):
+    def set_model_info(self, n_layers):
         self.n_layers = n_layers
-        self.n_weights = n_weights
     
     def plot_gradients(self, directory = "./plots", name = None):
         if not os.access(directory, os.F_OK):
             print(f'cannot access {directory}')
             return
-        name = f'{"output_" + str(time.time_ns()) if name is None else name}'
+        name = f'{"output_" + str(time.time_ns()) if name is None else name}.png'
 
         weights_gradients = dict()
         bias_gradients = dict()
 
-        for i in range(len(self.data)):
+        for i in range(len(self.data)-1):
             epoch = f'epoch-{i}'
-            for j in range(len(self.data[epoch])):
-                update = f'update-{j}'
-                for w in range(self.n_weights):
-                    weight = f'weights-gradient-{w}'
-                    if weights_gradients[w] is None:
-                        weights_gradients[w] = list()
-                    weights_gradients[w].append(np.asarray(self.data[epoch][update][weight]))
-                for b in range(self.n_layers):
-                    bias = f'bias-gradient-{b}'
-                    if bias_gradients[b] is None:
-                        bias_gradients[b] = list()
-                    bias_gradients[b].append(np.asarray(self.data[epoch][update][bias]))
+            for j in range(len(self.data[epoch])-1):
+                try:
+                    update = f'update-{j}'
+                    for w in range(self.n_layers-1):
+                        weight = f'weights-gradient-{w}'
+                        if weight not in weights_gradients:
+                            weights_gradients[weight] = list()
+                        weights_gradients[weight].append(np.asarray(self.data[epoch][update][weight]))
+                    for b in range(1, self.n_layers):
+                        bias = f'bias-gradient-{b}'
+                        if bias not in bias_gradients:
+                            bias_gradients[bias] = list()
+                        bias_gradients[bias].append(np.asarray(self.data[epoch][update][bias]))
+                except KeyError:
+                    break
+        
+        fig, axs = plt.subplots(2, self.n_layers-1, figsize = (12, 7))
+
+        # weights
+        for ax in range(self.n_layers-1):
+            weight = f'weights-gradient-{ax}'
+            weights_gradients[weight] = np.asarray(weights_gradients[weight])
+            for r in range(weights_gradients[weight][0].shape[0]):
+                for c in range(weights_gradients[weight][0].shape[1]):
+                    axs[0, ax].plot(weights_gradients[weight][:, r, c], label = f'w{ax}|r:{r},c:{c}')
+                    axs[0, ax].legend()
+        
+        # bias
+        for ax in range(1, self.n_layers):
+            bias = f'bias-gradient-{ax}'
+            bias_gradients[bias] = np.asarray(bias_gradients[bias])
+            for r in range(bias_gradients[bias][0].shape[0]):
+                    axs[1, ax-1].plot(bias_gradients[bias][:, r, 0], label = f'b{ax}|r:{r}')
+                    axs[1, ax-1].legend()
+        
+        fig.savefig(directory + "/" + name, bbox_inches = "tight")
+
+plotter = Plotter()
+plotter.set_model_info(3)
+plotter.read_file("logs\output_1739372926601414700.txt")
+plotter.plot_gradients()
