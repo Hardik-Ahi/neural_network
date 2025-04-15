97df5196d7067d404997b516802ee373b80014f7
Hardik-Ahi <ahi.hardik@gmail.com>
2025-04-15 23:40:39 +0530

    add dataframe splitting utilities

--- main.py
+++ main.py
@@ -1,34 +1,21 @@
 import numpy as np
 from nn.model_classes import Layer, Model
-from nn.dataset_utils import and_gate_dataset, linear_regression_dataset, standardize_data
+from nn.dataset_utils import and_gate_dataset, linear_regression_dataset, standardize_data, equalize_classes, split_classes, split_data
 from nn.functions import der_leaky_relu, der_sigmoid, leaky_relu, sigmoid, mean_square_error, der_mean_square_error, mirror, der_mirror
 from nn.optimizers import SGD
 from nn.trainer import Trainer, RegressionTrainer
 from nn.plotter import Plotter
+import pandas as pd
 
-model = Model(mean_square_error, der_mean_square_error, 100)
-model.add_layer(Layer(1))  # input layer
-model.add_layer(Layer(1, mirror, der_mirror))
-model.compile()
+X_train, y_train = and_gate_dataset()
+df = pd.DataFrame(np.hstack((X_train, y_train)), columns = ['input_0', 'input_1', 'output'])
+
+df = pd.DataFrame([[1, 1, 'a'], [2, 2, 'b'], [3, 3, 'c'], [4, 4, 'a'], [5, 5, 'b'], [6, 6, 'c'], [7, 7, 'a']], columns = ['1', '2', 'output'])
+
 
 X_train, y_train = linear_regression_dataset(100)
-X_train = standardize_data(X_train)  # important!!!
-y_train = standardize_data(y_train)
-Plotter.plot_points(X_train, y_train)
-
-trainer = RegressionTrainer(model, SGD())
-model.show_weights()
-model.show_biases()
-
-trainer.train(X_train, y_train, 1, 0.02, 50)
-trainer.save_history("./logs", "regression")
-
-plotter = Plotter()
-plotter.set_model_layers(2)
-plotter.read_file('./logs/regression.txt')
-plotter.plot_regression(X_train, y_train, "./plots")
-
-X_test, y_test = linear_regression_dataset(50, slope = 1, intercept = -4)
-X_test = standardize_data(X_test)
-y_test = standardize_data(y_test)
-trainer.predict(X_test, y_test)
+df = pd.DataFrame(np.hstack((X_train, y_train)), columns = ['x', 'y'])
+print(df)
+train, test = split_data(df)
+print(train)
+print(test)
