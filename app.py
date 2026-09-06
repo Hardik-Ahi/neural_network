bd3f4913d394bac7fe18470c5f1996936a0b3aac
Hardik-Ahi <ahi.hardik@gmail.com>
2026-09-06 16:15:07 +0530

    first working state of streamlit UI

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+st.set_page_config(
+    page_title="Neural Network Demonstration",
+    layout="wide"  # Turns on wide mode to remove huge margins
+)
+
 st.title("Neural Network from Scratch", 
   text_alignment="center", 
   icon=":material/network_node:")
@@ -39,11 +44,16 @@ from nn.functions import BinaryLoss, leaky_relu, der_leaky_relu, sigmoid, der_si
 
 st.header("Model")
 
-model = Model(BinaryLoss(), 1)
-model.add_layer(Layer(2))
-model.add_layer(Layer(2, leaky_relu(), der_leaky_relu()))
-model.add_layer(Layer(1, sigmoid(), der_sigmoid))
-model.compile()
+if "model" not in st.session_state:
+    # Replace this dict with your actual Model or Layer object instance
+    model = Model(BinaryLoss(), 1)
+    model.add_layer(Layer(2))
+    model.add_layer(Layer(2, leaky_relu(), der_leaky_relu()))
+    model.add_layer(Layer(1, sigmoid(), der_sigmoid))
+    model.compile()
+    st.session_state.model = model
+
+model = st.session_state.model
 
 # Display model layers as table
 layers = pd.DataFrame(columns=["Layer", "Number of Neurons", "Activation Function"])
@@ -53,6 +63,7 @@ for i, layer in enumerate(model.layers):
 st.dataframe(layers, hide_index=True)
 
 # show weights and biases of model via checkbox
+# move this to a sidebar to check in before and after training.
 show_weights_biases = st.checkbox("Show Weights and Biases")
 
 if show_weights_biases:
@@ -83,6 +94,7 @@ st.header("Training")
 
 trainer = Trainer(model, SGD())
 X_train, y_train = train_data.iloc[:, :-1].values, train_data.iloc[:, -1].values
+y_train = y_train.reshape(y_train.shape[0], 1)  # reshape to column vector
 
 # input fields for training
 batch_size = st.number_input("Batch Size (1 - 32)", min_value=1, max_value=32, value=1, step=1)
@@ -93,3 +105,39 @@ if st.button("Train Model"):
   with st.spinner("Training in progress..."):
     trainer.train(X_train, y_train, batch_size, learning_rate, epochs = epochs)
   st.success("Training completed!")
+
+  trainer.save_history('./logs', 'batch_size_1')
+  model.save_weights('./models', 'batch_size_1')
+
+# PLOT
+from nn.plotter import Plotter
+
+st.header("Training History")
+
+plotter = Plotter()
+
+if st.button("Plot History"):
+  with st.spinner("Reading log file..."):
+    plotter.read_file('./logs/batch_size_1.txt')
+
+  with st.spinner("Plotting gradients..."):
+    plotter.plot_gradients('./plots', 'batch_size_1', 700)
+    st.image('./plots/gradients_batch_size_1.png', caption='Gradients', use_container_width=True)
+
+  with st.spinner("Plotting weights..."):
+    plotter.plot_weights('./plots', 'batch_size_1', 700)
+    st.image('./plots/weights_batch_size_1.png', caption='Weights', use_container_width=True)
+
+  with st.spinner("Plotting accuracy..."):
+    plotter.plot_score('./plots', 'batch_size_1', 700)
+    st.image('./plots/score_batch_size_1.png', caption='Accuracy', use_container_width=True)
+
+  with st.spinner("Plotting outputs..."):
+    plotter.plot_predictions(X_train, "./plots", "batch_size_1")
+    st.image('./plots/predictions_batch_size_1.png', caption='Predictions', use_container_width=True)
+
+  with st.spinner("Plotting loss landscape..."):
+    plotter.plot_contours(trainer, X_train, y_train, "./plots", "batch_size_1")
+    st.image('./plots/contours_batch_size_1.png', caption='Loss Landscape', use_container_width=True)
+
+  st.success("Plots generated!")
